9dceaf7a9799bf17458a415fec71083f45d05127
sevenui <2179744658@qq.com>
2024-05-18 17:25:17 +0800

    前端环境搭建，登入页面及首页页面初步搭建搭建

--- weblog-vue3/src/main.ts
+++ weblog-vue3/src/main.ts
@@ -1,14 +1,22 @@
-import './assets/main.css'
+import '@/assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
-import App from './App.vue'
-import router from './router'
+import App from '@/App.vue'
+//导入路由
+import router from '@/router'
+// 导入 Element Plus 图标
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 const app = createApp(App)
 
 app.use(createPinia())
+//应用路由
 app.use(router)
 
 app.mount('#app')
+// 引入图标
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
--- weblog-vue3/src/router/index.ts
+++ weblog-vue3/src/router/index.ts
@@ -1,22 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Index from '@/pages/frontend/index.vue'
+import Login from '@/pages/admin/login.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      component: Index,
+      meta:{
+        title:"weblog 首页"
+      }
+    },{
+      path: '/login', // 登录页
+      component: Login,
+      meta: {
+          title: 'Weblog 登录页'
+      }
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
   ]
 })
 
